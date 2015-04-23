762b06862a633ef03b608fbed4e181f911137e03
jjfly12 <jjfly12@gmail.com>
2015-04-23 12:07:06 -0700

    Create NumButton.java

---
+++ 4-22-15/gui/NumButton.java
@@ -0,0 +1,46 @@
+package gui;
+
+import javax.swing.JButton:
+
+public class NumButton extends JButton implements MouseListener
+{
+  int number;
+  
+  public NumButton(int num)
+  {
+    super();
+    this.number = num;
+		this.setBackground(Color.WHITE);
+		this.setPreferredSize(new Dimension(15, 15));
+		this.setForeground(Color.BLACK);
+	}
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}

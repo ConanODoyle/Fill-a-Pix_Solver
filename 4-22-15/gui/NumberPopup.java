7340b9ab7534fc53f30631d1334a842442fe96dd
jjfly12 <jjfly12@gmail.com>
2015-04-23 11:26:34 -0700

    Create NumberPopup.java
    I'm trying to learn how to do this

---
+++ 4-22-15/gui/NumberPopup.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javax.swing.Jpanel;
+import javax.swing.Jbutton;
+
+public class NumberPopup
+{
+  JFrame mainFrame = new JFrame("Pick one!");
+  JPanel content = new JPanel()
+  NumButton number = new NumButton(1);
+  int posX;
+  int posY;
+  
+  public NumberPopup(int x, int y)
+  {
+    for(int i=0;i<10;i++)
+    {
+    }
+    number.addMouseListener(New MouseListener())
+    {
+    }
+  }
+}

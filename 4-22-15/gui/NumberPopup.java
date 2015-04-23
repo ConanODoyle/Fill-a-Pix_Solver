75a186b185285615682edef50a37bb42ce1850f6
jjfly12 <jjfly12@gmail.com>
2015-04-23 14:59:06 -0700

    Update NumberPopup.java

--- 4-22-15/gui/NumberPopup.java
+++ 4-22-15/gui/NumberPopup.java
@@ -7,17 +7,17 @@ public class NumberPopup
 {
   JFrame mainFrame = new JFrame("Pick one!");
   JPanel content = new JPanel()
-  NumButton number = new NumButton(1);
   int posX;
   int posY;
-  
-  public NumberPopup(int x, int y)
+  GUI gui;
+  public NumberPopup(GUI gui,int x, int y)
   {
+    this.gui = gui;
+    mainFrame.setContentPane(content);
+    
     for(int i=0;i<10;i++)
     {
-    }
-    number.addMouseListener(new MouseListener())
-    {
+      content.add(new NumButton(i,gui));
     }
   }
 }

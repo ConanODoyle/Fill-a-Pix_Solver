dfb457e0a918ccf6a677aa5ecb436b0659efd42d
jjfly12 <jjfly12@gmail.com>
2015-04-24 14:29:17 -0700

    Update GUI.java

--- 4-22-15/gui/GUI.java
+++ 4-22-15/gui/GUI.java
@@ -13,28 +13,42 @@ public class GUI {
 	private JFrame frame;
 	private JPanel buttonPanel;
 	private Board board;
+	private int[][] grid;
 	private String boardState = "e"; //e for edit, s for slow solve
 	
 	public GUI()
 	{
-		frame = new JFrame("Solver stuff");
-		
 	}
-	public void init(Board board) throws InterruptedException{
-		this.board = board;
+	public void init() throws InterruptedException{
 		StartMenu init = new StartMenu(this);
-		frame.add(init);
 	}
 	
-	public void openGrid(){
-		
+	public void openGUI()
+	{
+		frame = new JFrame("Grids!");
+		buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(gridheight,gridwidth));
+		for(int i=0;i<gridheight;i++)
+		{
+			for(int j=0;j<this.gridwidth;j++)
+			{
+				buttonPanel.add(new GridButton(this,null,j,i));
+			}
+		}
+		frame.pack();
+		frame.setVisible();
 	}
 	
-	public void setDimensions(){
-		
+	public void setDimensions(int x, int y){
+		this.gridwidth = x;
+		this.gridheight = y;
 	}
 	
 	public void setButtonValue(int x, int y, int value){ //called by NumPanel NumButtons
 		
+	}
+	public void solve()
+	{
+		//board class
 	}
 }

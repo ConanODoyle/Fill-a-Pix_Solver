82c0cfec8d91a211dfeb85ead95ab3868f13af19
ConanODoyle <this.is.not.a.email.account@gmail.com>
2015-04-23 18:47:00 -0700

    Update StartMenu
    Added completed preliminary start menu buttons

--- 4-22-15/gui/StartMenu.java
+++ 4-22-15/gui/StartMenu.java
@@ -14,15 +14,21 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 public class StartMenu {
-	JFrame initFrame = new JFrame("Start");
-	JPanel contentPanel = new JPanel();
-	JPanel initPanel = new JPanel();
-	JPanel titlePanel = new JPanel();
-	JButton file = new JButton("Select File");
-	JButton grid = new JButton("Input Puzzle");
-	GUI gui;
+	private JFrame initFrame = new JFrame("Start");
+	private JPanel contentPanel = new JPanel();
+	private JPanel initPanel = new JPanel();
+	private JPanel titlePanel = new JPanel();
+	private JButton file = new JButton("Select File");
+	private JButton grid = new JButton("Input Puzzle");
+	private GUI gui;
+	private int isClicking = 0;
+	private int isOverButton = 0;
+	
+	private static final Border UNSELECTED = BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createEmptyBorder(5, 10, 5, 10));
+	private static final Border SELECTED = BorderFactory.createCompoundBorder(BorderFactory.createLoweredBevelBorder(), BorderFactory.createEmptyBorder(5, 10, 5, 10));
 	
 	public StartMenu(GUI gui){	
 		this.gui = gui;	
@@ -41,49 +47,87 @@ public class StartMenu {
 		initPanel.setLayout(new BoxLayout(initPanel, BoxLayout.Y_AXIS));
 		initPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder(""), BorderFactory.createEmptyBorder(15, 5, 15, 5)));
 		
-		file.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createEmptyBorder(5, 10, 5, 10)));
-		grid.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+		file.setBorder(UNSELECTED);
 		file.setFocusable(false);
-		grid.setFocusable(false);
-		
 		file.setAlignmentX(Panel.CENTER_ALIGNMENT);
-		grid.setAlignmentX(Panel.CENTER_ALIGNMENT);
 		file.setMargin(new Insets(15, 15, 15, 15));
-		grid.setMargin(new Insets(15, 15, 15, 15));
 		file.setBackground(new Color(230,230,230));
+
+		grid.setBorder(UNSELECTED);
+		grid.setFocusable(false);
+		grid.setAlignmentX(Panel.CENTER_ALIGNMENT);
+		grid.setMargin(new Insets(15, 15, 15, 15));
 		grid.setBackground(new Color(230,230,230));
 		
 		file.addMouseListener(new MouseListener(){
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
 			}
 
 			@Override
 			public void mouseEntered(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
+				isOverButton = 1;
+				if (isClicking == 1){
+					file.setBorder(SELECTED);
+				}
 			}
 
 			@Override
 			public void mouseExited(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
+				isOverButton = 0;
+				file.setBorder(UNSELECTED);
 			}
 
 			@Override
 			public void mousePressed(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
+				isClicking = 1;
+				file.setBorder(SELECTED);
 			}
 
 			@Override
 			public void mouseReleased(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
+				if (isClicking == 1 && isOverButton == 1){
+					System.out.println("file");
+				}
+				isClicking = 0;	
+				file.setBorder(UNSELECTED);
 			}
 		});
+		grid.addMouseListener(new MouseListener(){
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				isOverButton = 2;
+				if (isClicking == 2){
+					grid.setBorder(SELECTED);
+				}
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				isOverButton = 0;
+				grid.setBorder(UNSELECTED);
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				isClicking = 2;
+				grid.setBorder(SELECTED);
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				if (isClicking == 2 && isOverButton == 2){
+					System.out.println("grid");
+				}
+				isClicking = 0;	
+				grid.setBorder(UNSELECTED);
+			}
+		});
+		
 		
 		initPanel.add(file);
 		JPanel spacer = new JPanel();

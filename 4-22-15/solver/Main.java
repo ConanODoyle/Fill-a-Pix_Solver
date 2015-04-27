810615b57e0764a6311bf48586a4c4765400ba78
ConanODoyle <this.is.not.a.email.account@gmail.com>
2015-04-27 14:09:59 -0700

    No change

--- 4-22-15/solver/Main.java
+++ 4-22-15/solver/Main.java
@@ -6,10 +6,12 @@ import java.io.File;
 
 public class Main {
 	public static void main(String[] args) throws Exception{
+		
 		Board n = new Board(new File("puzzle1"));
 		n.runmain();
 		
 		GUI gui = new GUI();
 		gui.init(null);
+		
 	}
 }
